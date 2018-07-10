1ccd1887b28f2554ede9684bae870ef233827222
Thiago Leite <thiago.leite@nad1.com.br>
2018-07-10 16:25:37 -0300

    update App to fetch api

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    data: [],
+    error: null,
+    loading: true
+  };
+
+  componentDidMount() {
+    fetch("/api/posts")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        this.setState({
+          error,
+          loading: false
+        });
+      });
+  }
+
+  renderPosts() {
+    const { data, loading } = this.state;
+
+    if (loading) {
+      return <p>loading...</p>;
+    }
+    return (
+      <ul>
+        {data.map(post => (
+          <li>
+            <b>title:</b> {post.title}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +55,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">{this.renderPosts()}</p>
       </div>
     );
   }
